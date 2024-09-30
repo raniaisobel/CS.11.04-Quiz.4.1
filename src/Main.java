53d3db44b14266d0ea90649f161b6dbfada88314
Rania <rania.isobel@gmail.com>
2024-09-30 13:39:13 +0800

    In class IDE quiz

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,17 @@ public class Main {
 
     /** 1. Write a method called numberOfVowels that returns the number of vowels in a string.
      Consider a, e, i, o, u as vowels. **/
-    
+    public static int numberOfVowels(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+                    c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
+                count++;
+            }
+        }
+        return count;
+    }
 
     /** 2. Write a method called notDivisibleBy235 that finds the number of natural numbers not exceeding N
      and not divisible by any of the numbers [2, 3, 5].
@@ -14,6 +24,15 @@ public class Main {
      4 - divides integer by 2
      5 - divides integer by 5
      Answer: 1 (only one number doesn't divide integer by any of 2, 3, 5) **/
+    public static int notDivisibleBy235(int num) {
+        int result = 0;
+        for (int i = 1; i <= num; i++) {
+            if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0) {
+                result++;
+            }
+        }
+        return result;
+    }
 
 
     /** 3. Write a method called camelCaseMe so that it converts dash/underscore delimited words into camel casing.
@@ -24,6 +43,22 @@ public class Main {
      "the-stealth-warrior" gets converted to "theStealthWarrior"
      "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
      "The_Stealth-Warrior" gets converted to "TheStealthWarrior" **/
+    public static String camelCaseMe(String str) {
+        str = str.replace("-", "_");
+        String[] words = str.split("_");
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < words.length; i++) {
+            if (i == 0) {
+                result.append(words[i]);
+            } else {
+                result.append(words[i].substring(0, 1).toUpperCase() + words[i].substring(1));
+            }
+        }
+        return result.toString();
+    }
+ }
 
 
-}
+
+
+
